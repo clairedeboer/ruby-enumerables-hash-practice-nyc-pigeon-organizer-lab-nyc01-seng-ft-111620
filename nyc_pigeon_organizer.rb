211db3f104d0534e915c9fe8c404199710e014dc
Claire DeBoer <claire.deboer@gmail.com>
2020-11-02 23:20:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,8 @@ def nyc_pigeon_organizer(data)
         if !final_object[name][key]
           final_object[name][key] = []
         end
-        final_object[name][key].push(inner_key.to_s)
+        named_pigeon_attributes = final_object[name][key]
+        named_pigeon_attributes.push(inner_key.to_s)
       end
     end
   end
