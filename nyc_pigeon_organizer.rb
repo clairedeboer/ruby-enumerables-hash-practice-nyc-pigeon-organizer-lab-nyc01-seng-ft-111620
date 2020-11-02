f139673b1f321880d1daa11accb201fe8bf0d8e1
Claire DeBoer <claire.deboer@gmail.com>
2020-11-02 23:16:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,12 @@
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_object({}) do |(key, value), final_array|
+  final_results = data.each_with_object({}) do |(key, value), final_object|
     value.each do |inner_key, names|
       names.each do |name|
-        if !final_array[name]
-          final_array[name] = {}
+        if !final_object[name]
+          final_object[name] = {}
         end
-        if !final_array[name][key]
-          !final_array[name][key] = []
+        if !final_object[name][key]
+          !final_object[name][key] = []
         end
         final_array[name][key].push(inner_key.to_s)
       end
