228fb761f771e1153b4bf4c0dd2777ac3efabdbd
Claire DeBoer <claire.deboer@gmail.com>
2020-11-03 00:05:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) { |(outer_key, value), output| 
   # outer_key == color || gender || lives
     inner_keys = data[outer_key].keys
-    #  --> inner_keys
+
     inner_keys.each { |inner_key| 
       names = data[outer_key][inner_key]
       names.each { |name| 
